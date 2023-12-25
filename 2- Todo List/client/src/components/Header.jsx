8dbc8f203f6a6f59190f24714f7ac52993eaaa08
selimcanozgur <webisdeveloper@gmail.com>
2023-12-25 20:30:36 +0300

    todo app remove

--- 2- Todo List/client/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="text-center text-5xl  uppercase text-white mt-16 font-bold ">
-      Todo List
-    </header>
-  );
-}
-
-export default Header;
